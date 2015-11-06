2cebbd69543683b4306db164061237af8dae4770
wangyan <wyvernnot@gmail.com>
2015-11-06 11:10:30 +0800

    Readme

--- README.md
+++ README.md
@@ -2,9 +2,24 @@
 ![](https://img.shields.io/travis/wyvernnot/fs-animation.svg)
 ![](https://img.shields.io/coveralls/wyvernnot/fs-animation.svg)
 
-Realitime file system change animation which can also be played back.
+Realitime file system change animation which can also be played back. Built on [Immutable.JS](https://github.com/facebook/immutable-js/)
 
-## Example 1 - Watch docker directory
+## Usage 
+
+```sh
+npm start
+```
+
+## Test
+
+```sh
+npm test
+```
+
+## Example - Watch docker directory
 
 ![](doc/screenshot.png)
+
+[Immutable.JS](https://github.com/facebook/immutable-js/) 真是个奇怪的鬼 :smiling_imp: ，一直不知道它是干什么的，理解起来也巨[费劲](http://www.zhihu.com/question/28016223)，
+后来发现这货就是为这个项目而生的啊！
 
--- server.js
+++ server.js
@@ -4,49 +4,52 @@
 
 var http = require('http');
 var express = require('express');
-var app = express();
 var path = require('path');
 var WebSocketServer = require('ws').Server;
 var chokidar = require('chokidar');
-
 var History = require('./lib/History');
 
+// bootstraping
+
+var app = express();
 var server = http.createServer();
 var wss = new WebSocketServer({
   server: server
 });
-
 var watcher = chokidar.watch('/var/lib/docker/', {
   ignored: [/[\/\\]\./,/docker\/aufs/],
   ignoreInitial: true
 });
+var history = new History(watcher);
 
-var h = new History(watcher);
-
-h.on('update', function () {
+history.on('update', function () {
   wss.clients.forEach(function each(client) {
     client.send('update');
   });
 });
 
+// serve static files
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+// route definition
+
 app.get('/tree/latest', function (req, res) {
-  res.json(h.current.toJSON());
+  res.json(history.current.toJSON());
 });
 
 app.get('/tree/size', function (req, res) {
-  res.json(h.history.length);
+  res.json(history.history.length);
 });
 
 app.get('/tree/history', function (req, res) {
-  res.json(h.history.map(function (no) {
+  res.json(history.history.map(function (no) {
     return no.toJSON();
   }));
 });
 
 app.get('/tree/:id', function (req, res) {
-  res.json(h.history[req.params.id].toJSON());
+  res.json(history.history[req.params.id].toJSON());
 });
 
 server.on('request', app);
