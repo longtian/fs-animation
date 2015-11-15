e1fd2a56f87cd6f9527a4b69a32ea4469e051807
wyvern <wyvernnot@gmail.com>
2015-11-15 15:55:18 +0800

    Fix #1 deal with wrong add sequence

---
+++ test/testSequence.js
@@ -0,0 +1,29 @@
+/**
+ * Created by yan on 15-11-15.
+ */
+
+var History = require('../lib/History');
+var MockFSWatcher = require('../lib/MockFSWatcher');
+var path = require('path');
+var deepEqual = require('assert').deepEqual;
+
+describe('History', function () {
+  it('Meets wrong add sequence', function (done) {
+    var watcher = new MockFSWatcher(path.join(__dirname, 'testSequence.txt'));
+    var h = new History(watcher);
+    watcher.on('end', function () {
+      deepEqual(h.history[0].toJSON(), {
+        "a": {
+          "b": {}
+        }
+      });
+      deepEqual(h.current.toJSON(), {
+        "a": {
+          "b": {},
+          "c": {}
+        }
+      });
+      done();
+    });
+  });
+})
